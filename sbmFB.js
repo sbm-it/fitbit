5b292feb13e7470fcde26457656f8608273346a9
Jonas Almeida <jalmeida@mathbiol.org>
2016-10-12 11:41:52 -0400

    oauth connect working

--- sbmFB.js
+++ sbmFB.js
@@ -1,3 +1,16 @@
 console.log('sbmFB.js loaded');
 
+(function(){
+    var fb = {}
+    fb.connect=function(scopes){
+        scopes = scopes || ""  // defayl is empty string
+        url='https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22834T&redirect_uri='+encodeURIComponent((location.origin+location.pathname).slice(0,-1))+'&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800'
+        4
+
+    }
+
+    // ini
+    fb.connect()
+})()
+
 
