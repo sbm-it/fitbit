b996c69e8a6186239f5c4361b5ef1f228d09e38c
Jonas Almeida <jalmeida@mathbiol.org>
2016-10-19 11:56:52 -0400

    got token

--- sbmFB.js
+++ sbmFB.js
@@ -2,12 +2,35 @@ console.log('sbmFB.js loaded');
 
 (function(){
     fb = {}
+    
     fb.connect=function(scopes){
         scopes = scopes || ""  // defayl is empty string
         fb.url='https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22834T&redirect_uri='+encodeURIComponent((location.origin+location.pathname).slice(0,-1))+'&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800'
         window.open(fb.url)
 
     }
+
+    fb.UI=function(div){ // assemble user interface
+        if(typeof(div)=='string'){
+            div=document.getElementById(div)
+        }
+        if(typeof(div)=='string'){
+            throw('target division for UI assembly not found')
+        }
+        // UI div found
+        h='<h3>[<a href="https://github.com/sbm-it/fitbit" target="_blank">source</a>] FitBit data:</h3>'
+        h+='<li id="accessToken"> Assess Token: <span style="color:orange">'+fb.parms.access_token+'</span> </li>'
+        location.hash="" // clear token from hash
+        
+
+
+        div.innerHTML=h
+        4
+        4
+
+    }
+
+
 
     // ini
     if(location.hash.length==0){
@@ -20,7 +43,11 @@ console.log('sbmFB.js loaded');
             fb.parms[p[0]]=p[1]
         })
         //location.hash="" // hide login parms
-        4
+        if(fb.parms.access_token){ // if logged in, assemble UI
+            if(document.getElementById('appSpace')){
+                fb.UI(appSpace)
+            }
+        }
     }
     //
 })()
